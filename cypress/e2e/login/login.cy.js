9b8458e69dc6fc88ac89d3d65ad8589f0143daf7
pedrohbastos <peedrobastos@hotmail.com>
2026-03-24 01:37:25 -0300

    fix: move intercept into stub test

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -12,10 +12,14 @@ describe('Login', () => {
        cy.login('ana@email.com', 'Senha123')
        
   })
-  it('Verifica mensagem de falha no login', () => {
-        cy.get('[data-test="submit-button"]').click()
-        cy.contains('É necessário informar um endereço de email').should('be.visible')
-        cy.contains('Insira sua senha').should('be.visible')
+   it('Deve falhar mesmo que os campos sejam preenchidos corretamente', () => {
+        cy.intercept('POST', 'https://adopet-api-i8qu.onrender.com/adotante/login', {
+            statusCode: 400,
+        }).as('stubPost')
+
+        cy.login('ana@email.com', 'Senha123')
+        cy.wait('@stubPost')
+        cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
     })
 
     it('Deve falhar mesmo que os campos sejam preenchidos corretamente', ()=> {
